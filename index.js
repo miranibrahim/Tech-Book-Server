aaac08a5d2459324d65aedd3c010900a0fd62e66
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-11-28 01:45:58 +0600

    product post method implemented

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -17,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,65 +26,94 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db("TechBookDB").collection("products");
-    const menuCollection = client.db("TechBookDB").collection("menu");
+    const userCollection = client.db("TechBookDB").collection("users");
     const reviewCollection = client.db("TechBookDB").collection("reviews");
     const cartCollection = client.db("TechBookDB").collection("carts");
     const paymentCollection = client.db("TechBookDB").collection("payments");
 
+    // -------------- MiddleWares -----------------------
+    const verifyToken = (req, res, next) => {
+      console.log(req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+          return res.status(401).send({ message: "forbidden" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      next();
+    };
+
+    // ----------------JWT token ------------------------
+    app.post("/jwt", (req, res) => {
+      const userInfo = req.body;
+      const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    //  ----------------for users -------------------------
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "user exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    
 
     // ---------products-----------------
-    // app.get("/products", async (req, res) => {
-    //   try {
-    //     const result = await productCollection.find({ status: 'accepted' }).sort({ timestamp: -1 }).toArray();
-    //     res.send(result);
-    //   } catch (error) {
-    //     console.error("Error fetching products:", error);
-    //     res.status(500).send("Internal Server Error");
-    //   }
-    // });
-    app.get('/products', async (req, res) => {
-      try {
-        const { search, page } = req.query;
-        const pageSize = 20;
-        const skip = (page - 1) * pageSize;
-  
-        // Build the query based on search term
-        const query = search ? { status: 'accepted', tags: { $in: [search] } } : { status: 'accepted' };
-  
-        // Fetch products with pagination
-        const products = await productCollection
-          .find(query)
-          .sort({ timestamp: -1 })
-          .skip(skip)
-          .limit(pageSize)
-          .toArray();
-  
-        res.json(products);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-        res.status(500).send('Internal Server Error');
-      }
+
+    app.get("/products", async (req, res) => {
+      const query = { status: "accepted"};
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const productItem = req.body;
+      const result = await productCollection.insertOne(productItem);
+      res.send(result);
     });
 
     app.get("/trending-products", async (req, res) => {
-      try {
-        // Fetch products that are accepted and featured
-        const featuredProducts = await productCollection
-          .find({ status: 'accepted', featured: true })
-          .toArray();
-    
-        // Sort products based on the number of votes in descending order
-        const trendingProducts = featuredProducts.sort((a, b) => b.upvoteCount - a.upvoteCount);
-    
-        res.send(trendingProducts);
-      } catch (error) {
-        console.error("Error fetching trending products:", error);
-        res.status(500).send("Internal Server Error");
-      }
+      const query = { status: "accepted", featured: true, };
+      const result = await productCollection.find(query).sort({ upvoteCount: -1 }).toArray();
+      res.send(result);
     });
-    // Send a ping to confirm a successful connection
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -91,11 +121,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("running!");
+});
 
-app.get('/', (req, res) => {
-    res.send('running!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
